20818b216611e463b273e56653bd08d5cbf69806
haosome6 <zhzjim@gmail.com>
2022-08-27 01:34:49 -0400

    load in original dataset

--- src/utils/custom_dataset.py
+++ src/utils/custom_dataset.py
@@ -2,14 +2,41 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 from PIL import Image
+from scipy.io import loadmat
+import pickle
+import numpy as np
 
 
 class MNISTOverCifar(Dataset):
     def __init__(self, root, train=True, download=True, transform=None):
         self.root = root
         self.transform = transform
-        self.mnist = datasets.MNIST(root, train=train, download=download)
-        self.cifar = datasets.CIFAR10(root, train=train, download=download)
+        self.train = train
+        # self.mnist = datasets.MNIST(root, train=train, download=download)
+        # self.cifar = datasets.CIFAR10(root, train=train, download=download)
+
+        mnist = loadmat('../../data/original_data/mnist-original.mat')
+        self.mnist_data, self.mnist_label = mnist['data'].T, mnist['label'][0]
+
+        self.cifar_data = None
+        if train:
+            # mnist
+            self.mnist_data, self.mnist_label = self.mnist_data[:50000,:], self.mnist_label[:50000]
+            # cifar
+            for i in range(1, 6):
+                with open('../../data/original_data/cifar-10-batches-py/data_batch_{}'.format(i), 'rb') as f:
+                    dict = pickle.load(f, encoding='bytes')
+                if self.cifar_data is None:
+                    self.cifar_data = dict[b'data']
+                else:
+                    self.cifar_data = np.vstack((self.cifar_data, dict[b'data']))
+        else:
+            # mnist
+            self.mnist_data, self.mnist_label = self.mnist_data[50000:60000,:], self.mnist_label[50000:60000]
+            # cifar
+            with open('../../data/original_data/cifar-10-batches-py/test_batch', 'rb') as f:
+                dict = pickle.load(f, encoding='bytes')
+            self.cifar_data = dict[b'data']
 
     def __len__(self):
         return min(len(self.mnist), len(self.cifar))
@@ -22,7 +49,9 @@ class MNISTOverCifar(Dataset):
         mnist_label, cifar_label = torch.tensor(mnist_label), torch.tensor(cifar_label)
 
         # transform mnist image to the same size as cifar image
-        mnist_img = transforms.Resize((32, 32), transforms.InterpolationMode.BILINEAR)(mnist_img)
+        # mnist_img = transforms.Resize((32, 32), transforms.InterpolationMode.NEAREST)(mnist_img)
+        edge_padding = torch.nn.ZeroPad2d(2)
+        mnist_img = edge_padding(mnist_img)
         mnist_img = transforms.Lambda(lambda x: x.repeat(3, 1, 1))(mnist_img)
 
         img = torch.where(mnist_img != 0, mnist_img, cifar_img)
